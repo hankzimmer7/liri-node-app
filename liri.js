a3ca48f93a8fd2d8d00814f1716cf604dabb0c43
hankzimmer7 <hankzimmer7@gmail.com>
2018-07-19 17:30:03 -0400

    adds spotify search functionality and ability to read random.txt

--- liri.js
+++ liri.js
@@ -1,53 +1,145 @@
+//Import required packages
 require("dotenv").config();
 var request = require("request");
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 var keys = require("./keys.js");
+var fs = require("fs");
 
-// var spotify = new Spotify(keys.spotify);
+//Get keys using keys.js
+var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+//Variables to store the user inputs
 var userCommand = process.argv[2];
+var userTerm = process.argv.slice(3).join(" ")
 
-if (userCommand === "my-tweets") {
+//Variable to store the information LIRI retrieves
+var resultsInfo = "";
 
-    // This will show your last 20 tweets and when they were created at in your terminal/bash window.
+//Function to append information to the log.txt
+function writeToLog(data) {
+    var divider = "\n-------------------------------------------\n";
 
-    var params = {
-        screen_name: 'realDonaldTrump'
-    };
-    client.get('statuses/user_timeline', params, function (error, tweets, response) {
-        if (!error) {
-            for (var i = 0; i < 20; i++) {
-                console.log("\nTweet " + i + ": " + tweets[i].text);
-                console.log("Created " + tweets[i].created_at);
+    //Append the data to the log.txt file
+    fs.appendFile("log.txt", "\nCommand run: " + userCommand + " " + userTerm + "\n" + data + divider, function (err) {
+        // If the code experiences any errors it will log the error to the console.
+        if (err) {
+            return console.log(err);
+        }
+        // Otherwise, it will print: "log.txt was updated!"
+        console.log("log.txt was updated!");
+    });
+}
+
+//If the user used the "do-what-it-says" command
+if (userCommand === "do-what-it-says") {
+
+    //Read the random.txt file and execute its command
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+
+        // Split the data by commas to get each command
+        var dataArray = data.split(",");
+
+        //Set the command terms based on the data within log.txt
+        userCommand = dataArray[0];
+        userTerm = dataArray[1];
+
+        console.log("\nRandom.txt says: " + data + "\nExecuting command.");
+
+        executeCommand();
+    });
+} else {
+    executeCommand();
+}
+
+// Function to execute the desired commands
+function executeCommand() {
+
+    //If the user entered the "my-tweets" command
+    if (userCommand === "my-tweets") {
+        // This will show your last 20 tweets and when they were created at in your terminal/bash window.
+        var params = {
+            screen_name: 'realDonaldTrump'
+        };
+        client.get('statuses/user_timeline', params, function (error, tweets, response) {
+            if (error) {
+                return console.log('Error occurred: ' + error);
             }
+
+            for (var i = 0; i < 20; i++) {
+                resultsInfo = resultsInfo.concat("\nTweet " + i + ": " + tweets[i].text + "\nCreated " + tweets[i].created_at + "\n");
+            }
+            console.log(resultsInfo);
+            writeToLog(resultsInfo);
+        });
+    }
+
+    //If the user entered the "spotify-this-song-command"
+    else if (userCommand === "spotify-this-song") {
+        var songTitle = userTerm;
+
+        //The default song is The Sign by Ace of Base
+        if (!songTitle) {
+            songTitle = "The Sign Ace of Base";
         }
-    });
-} else if (userCommand === "spotify-this-song") {
-    var songTitle = process.argv.slice(3).join(" ");
-    console.log(songTitle);
-} else if (userCommand === "movie-this") {
-    //Get the movie title and use plus signs instead of spaces
-    var movieTitle = process.argv.slice(3).join("+");
 
-    // Then run a request to the OMDB API with the movie specified
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
+        spotify.search({
+            type: 'track',
+            query: songTitle
+        }, function (error, data) {
+            if (error) {
+                return console.log('Error occurred: ' + err);
+            }
 
-    // This line is just to help us debug against the actual URL.
-    // console.log(queryUrl);
+            var songInfo = data.tracks.items[1];
 
-    request(queryUrl, function (error, response, body) {
+            resultsInfo = "\nArtist(s): " + songInfo.artists[0].name +
+                "\nSong Title: " + songInfo.name +
+                "\nAlbum: " + songInfo.album.name +
+                "\nFull Track Link: " + songInfo.external_urls.spotify +
+                "\nPreview Link: " + songInfo.preview_url + "\n";
 
-        // If the request is successful
-        if (!error && response.statusCode === 200) {
-            console.log("\nMovie Title: " + JSON.parse(body).Title);
-            console.log("Release Year: " + JSON.parse(body).Year);
-            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-            console.log("Country: " + JSON.parse(body).Country);
-            console.log("Language: " + JSON.parse(body).Language);
-            console.log("Plot: " + JSON.parse(body).Plot);
-            console.log("Actors: " + JSON.parse(body).Actors);
+            console.log(resultsInfo);
+            writeToLog(resultsInfo);
+        });
+
+    }
+
+    //If the user used the "movie-this" command
+    else if (userCommand === "movie-this") {
+        //Get the movie title and use plus signs instead of spaces so it can be used in the url
+        var movieTitle = userTerm;
+
+        // The default movie is Mr Nobody
+        if (!movieTitle) {
+            movieTitle = "Mr Nobody";
         }
-    });
+
+        // Then run a request to the OMDB API with the movie specified
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
+
+        request(queryUrl, function (error, response, body) {
+
+            // If the request is successful
+            if (!error && response.statusCode === 200) {
+                resultsInfo = "\nMovie Title: " + JSON.parse(body).Title +
+                    "\nRelease Year: " + JSON.parse(body).Year +
+                    "\nIMDB Rating: " + JSON.parse(body).imdbRating +
+                    "\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value +
+                    "\nCountry: " + JSON.parse(body).Country +
+                    "\nLanguage: " + JSON.parse(body).Language +
+                    "\nPlot: " + JSON.parse(body).Plot +
+                    "\nActors: " + JSON.parse(body).Actors + "\n";
+
+                console.log(resultsInfo);
+                writeToLog(resultsInfo);
+            }
+        });
+    }
 }
