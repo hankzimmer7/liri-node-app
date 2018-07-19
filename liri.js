a6debae1117bf7d82f6876719629ac04a0b71b0b
hankzimmer7 <hankzimmer7@gmail.com>
2018-07-19 00:25:12 -0400

    adds functionality for movie search

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+var request = require("request");
 var Twitter = require('twitter');
 var keys = require("./keys.js");
 
@@ -6,7 +7,6 @@ var keys = require("./keys.js");
 var client = new Twitter(keys.twitter);
 
 var userCommand = process.argv[2];
-console.log("User Command is: " + userCommand);
 
 if (userCommand === "my-tweets") {
 
@@ -23,13 +23,31 @@ if (userCommand === "my-tweets") {
             }
         }
     });
-}
-
-if (userCommand === "spotify-this-song") {
+} else if (userCommand === "spotify-this-song") {
     var songTitle = process.argv.slice(3).join(" ");
     console.log(songTitle);
-}
-if (userCommand === "movie-this") {
-    var movieTitle = process.argv.slice(3).join(" ");
-    console.log(movieTitle);
+} else if (userCommand === "movie-this") {
+    //Get the movie title and use plus signs instead of spaces
+    var movieTitle = process.argv.slice(3).join("+");
+
+    // Then run a request to the OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
+
+    // This line is just to help us debug against the actual URL.
+    // console.log(queryUrl);
+
+    request(queryUrl, function (error, response, body) {
+
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+            console.log("\nMovie Title: " + JSON.parse(body).Title);
+            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
+        }
+    });
 }
